b1ab5faa3e26ee083a26712b0d63633b2f0292a9
alliburgan <allison.burgan@stu.daviess.kyschools.us>
2021-12-14 13:10:50 -0600

    added textbox

--- app/src/main/java/com/allib/vocal/MainActivity.kt
+++ app/src/main/java/com/allib/vocal/MainActivity.kt
@@ -2,10 +2,22 @@ package com.allib.vocal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View) {
+        // Do something in response to button
+        val clickedText = findViewById<Button>(R.id.btnPizza)
+        val sentence = clickedText.text.toString()
+        findViewById<TextView>(R.id.txtSentence).apply{
+            text = sentence
+        }
     }
 }
