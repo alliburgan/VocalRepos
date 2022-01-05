aad37de1a6727f98369202252673e2b3516432f4
alliburgan <allison.burgan@stu.daviess.kyschools.us>
2022-01-05 14:32:04 -0600

    made food functional

--- app/src/main/java/com/allib/vocal/FoodActivity.kt
+++ app/src/main/java/com/allib/vocal/FoodActivity.kt
@@ -2,10 +2,41 @@ package com.allib.vocal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+var words = arrayListOf<String>()
+var sentence = ""
 class FoodActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food)
+    }
+
+    fun btnClick(view: View) {
+        val btnID = view.id
+        val clickedText = findViewById<Button>(btnID)
+
+        words.add(clickedText.text.toString())
+        makeSentence()
+    }
+
+    private fun makeSentence() {
+        sentence = ""
+        for (i in words.indices) {
+            sentence += words[i] + " "
+        }
+        findViewById<TextView>(R.id.txtSentence).apply {
+            text = sentence
+        }
+    }
+    fun clear(view: View){
+        words.clear()
+        makeSentence()
+    }
+
+    fun delete(view: View) {
+        words.removeAt(words.size-1)
+        makeSentence()
     }
 }
