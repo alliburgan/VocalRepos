ba388655f4e14476f4282c1de9d44c74590fa4e6
alliburgan <allison.burgan@stu.daviess.kyschools.us>
2022-01-05 13:01:21 -0600

    this is a test 2.0

--- app/src/main/java/com/allib/vocal/MainActivity.kt
+++ app/src/main/java/com/allib/vocal/MainActivity.kt
@@ -4,8 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,17 +11,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun openKeyboard(view: View) {
-        // Do something in response to button
-//val intent = Intent(this, Class.forName(btnTag).javaClass)
-//
- val btnTag: String = view.tag.toString()
-
+    fun openFoodKeyboard(view: View) {
         val intent = Intent(this, FoodActivity::class.java)
-
-
-
         startActivity(intent)
-
     }
 }
