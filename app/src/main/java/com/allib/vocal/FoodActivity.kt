362afd856e5068362ce6b02519068962cd6b6166
DalMat43 <mdalton0046@kctcs.edu>
2022-01-05 13:17:50 -0600

    matisseTest

--- app/src/main/java/com/allib/vocal/FoodActivity.kt
+++ app/src/main/java/com/allib/vocal/FoodActivity.kt
@@ -7,5 +7,7 @@ class FoodActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food)
+
+        //matisseTests
     }
 }
